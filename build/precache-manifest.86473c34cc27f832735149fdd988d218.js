a0fcb749c91ddef705bac7f8c310b8d402910a8e
Sachin <mittalsam98@gmail.com>
2020-06-12 18:47:07 +0530

    gitignore file correct

---
+++ build/precache-manifest.86473c34cc27f832735149fdd988d218.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b3667cd6d5cd21a87567ed0ad505e03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8afbafe7030b1db03343",
+    "url": "/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "0833a680b5819dee2899",
+    "url": "/static/css/main.24dc54b6.chunk.css"
+  },
+  {
+    "revision": "8afbafe7030b1db03343",
+    "url": "/static/js/2.99a17b66.chunk.js"
+  },
+  {
+    "revision": "8b0b0209d23e8c647d2b956557587522",
+    "url": "/static/js/2.99a17b66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0833a680b5819dee2899",
+    "url": "/static/js/main.0685f38f.chunk.js"
+  },
+  {
+    "revision": "ff71c0547b8dfeaa6e05",
+    "url": "/static/js/runtime-main.d2f64a90.js"
+  },
+  {
+    "revision": "fdcb54312a6bfb4bda094ce57c070e55",
+    "url": "/static/media/bstack.fdcb5431.png"
+  },
+  {
+    "revision": "b6bb8a9b74648f4f93eb689a8e5d0caf",
+    "url": "/static/media/des.b6bb8a9b.png"
+  }
+]);
