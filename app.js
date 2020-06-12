3fc91268d5fcdbad48135a9f8ff820d76739eda3
Sachin <mittalsam98@gmail.com>
2020-06-12 18:39:15 +0530

    bugs fixed

--- app.js
+++ app.js
@@ -50,10 +50,10 @@ mongoose
 
   if(process.env.NODE_ENV=='production'){
     // console.log('jeee',__dirname);
-    app.use(express.static('/build'));
+    app.use(express.static('./build'));
     // app.use(express.static(path.join(__dirname, 'build')));
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname+'/build/index.html'));
+      res.sendFile(path.join(__dirname,'build','index.html'));
     });
   }
 
